1e56fdcf8c212b0e2933f71a1d5ff365eac3883f
warmthdawn <warmth06@sina.com>
2021-03-26 10:27:09 +0800

    第五次作业

---
+++ Exercise5/OrderManager/OrderService.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OrderManager
+{
+
+    public static class OrderService
+    {
+        private static List<Order> _orders = new List<Order>();
+        #region 查询（默认排序）
+        public static IEnumerable<Order> QueryById(int orderId)
+            => QueryBy(o => o.OrderId == orderId);
+        public static IEnumerable<Order> QueryByName(string productName)
+            => QueryBy(o =>
+               o.Details.Where(d => d.ProductName == productName).Any()
+            );
+        public static IEnumerable<Order> QueryByCustomer(string customerName)
+            => QueryBy(o => o.CustomerName == customerName);
+        public static IEnumerable<Order> QueryByAmount(decimal minAmount, decimal maxAmount)
+        {
+            if (maxAmount < minAmount)
+                throw new ArgumentException("最大值应该比最小值要大！");
+            return QueryBy(o => o.OrderAmount >= minAmount && o.OrderAmount <= maxAmount);
+        }
+        private static IEnumerable<Order> QueryAll()
+        {
+            return from o in _orders
+                   orderby o.OrderAmount
+                   select o;
+        }
+        private static IEnumerable<Order> QueryBy(Predicate<Order> predicate)
+        {
+
+            if (predicate == null)
+                throw new ArgumentException("参数不能为null");
+            return from o in _orders
+                   where predicate(o)
+                   orderby o.OrderAmount
+                   select o;
+        }
+        #endregion
+
+        #region 查询（自定义排序）
+        public static IEnumerable<Order> QueryById(int orderId, Comparison<Order> orderbyCompare)
+            => QueryBy(o => o.OrderId == orderId, orderbyCompare);
+        public static IEnumerable<Order> QueryByName(string productName, Comparison<Order> orderbyCompare)
+            => QueryBy(o =>
+                    o.Details.Where(d => d.ProductName == productName).Any()
+                , orderbyCompare);
+        public static IEnumerable<Order> QueryByCustomer(string customerName, Comparison<Order> orderbyCompare)
+            => QueryBy(o => o.CustomerName == customerName, orderbyCompare);
+        public static IEnumerable<Order> QueryByAmount(decimal minAmount, decimal maxAmount, Comparison<Order> orderbyCompare)
+        {
+            if (maxAmount < minAmount)
+                throw new ArgumentException("最大值应该比最小值要大！");
+            return QueryBy(o => o.OrderAmount >= minAmount && o.OrderAmount <= maxAmount, orderbyCompare);
+        }
+
+        private static IEnumerable<Order> QueryBy(Predicate<Order> predicate, Comparison<Order> orderbyCompare)
+        {
+
+            if (orderbyCompare == null || predicate == null)
+                throw new ArgumentException("参数不能为null");
+            return from o in _orders
+                   where predicate(o)
+                   orderby orderbyCompare
+                   select o;
+        }
+
+        private static IEnumerable<Order> QueryAll(Comparison<Order> orderbyCompare)
+        {
+            return from o in _orders
+                   orderby orderbyCompare
+                   select o;
+        }
+        #endregion
+
+        public static bool AddOrder(Order order)
+        {
+            if (order == null || order.Details == null)
+                throw new ArgumentException("参数不能为null");
+            //检查是否已经存在
+            if (_orders.Where(o => o.OrderId == order.OrderId).Any())
+            {
+                return false;
+            }
+
+            _orders.Add(order);
+            return true;
+        }
+
+        public static void EditOrder(Order order)
+        {
+            if (order == null)
+                throw new ArgumentException("参数不能为null");
+
+            var currentOrder = _orders.Where(o => o.OrderId == order.OrderId).FirstOrDefault();
+            if (currentOrder == null)
+                throw new InvalidOperationException("要编辑的订单不存在！");
+
+
+            if (!string.IsNullOrEmpty(order.CustomerName))
+                currentOrder.CustomerName = order.CustomerName;
+            if (order.Details != null && order.Details.Length > 0)
+                currentOrder.Details = order.Details;
+            if (order.OrderAmount > 0)
+                currentOrder.OrderAmount = order.OrderAmount;
+
+        }
+
+        public static void DeleteOrder(int orderId)
+        {
+            var currentOrder = _orders.Where(o => o.OrderId == orderId).FirstOrDefault();
+            if (currentOrder == null)
+                throw new InvalidOperationException("要删除的订单不存在！");
+
+            _orders.Remove(currentOrder);
+
+        }
+
+
+    }
+}
---
+++ Exercise5/OrderManager/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace OrderManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderService.AddOrder(new Order(0, "软件工程概论", 1, "法外狂徒张三", 114));
+            OrderService.AddOrder(new Order(1, "数据库系统", 2, "匿名群友", 514));
+            OrderService.AddOrder(new Order(2, "罗翔说刑法", 1, "罗翔", 43));
+            OrderService.AddOrder(new Order(3, "计算机网络", 1, "法外狂徒张三", 233));
+            OrderService.AddOrder(new Order(4, "热爱学习的贵龙", 10,
+                new OrderDetails("软件工程概论", 1),
+                new OrderDetails("Unity开发教程", 1),
+                new OrderDetails("教你画像素画", 1)
+            ));
+
+            Console.WriteLine("-------按价格查询， 按订单号排序-------");
+            OrderService.QueryByAmount(100, decimal.MaxValue, (a, b) => a.OrderId - b.OrderId).Print();
+            Console.WriteLine("-------按顾客查询-------");
+            OrderService.QueryByCustomer("法外狂徒张三").Print();
+            Console.WriteLine("-------按名称查询-------");
+            OrderService.QueryByName("软件工程概论").Print();
+
+            Console.WriteLine("-------修改订单-------");
+
+            OrderService.EditOrder(new Order(1, "群友不匿名了", 0, null));
+            OrderService.QueryById(1).Print();
+
+            OrderService.DeleteOrder(3);
+            OrderService.DeleteOrder(1);
+
+
+        }
+
+    }
+
+    public static class OrderExtensions
+    {
+        public static void Print(this IEnumerable<Order> orders)
+        {
+            foreach (var item in orders)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
